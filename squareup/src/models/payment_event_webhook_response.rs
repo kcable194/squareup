904f0fb3a9a8669379aad7ea4759e2b858a8e68d
Keaton Cable <70652641+kcable194@users.noreply.github.com>
2024-02-27 08:36:35 -0500

    Update payment_event_webhook_response.rs

--- squareup/src/models/payment_event_webhook_response.rs
+++ squareup/src/models/payment_event_webhook_response.rs
@@ -6,14 +6,11 @@ use super::{enums::PaymentEventPaymentEventType, PaymentEventData};
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct PaymentEventWebhookResponse {
     /// The ID of the target merchant associated with the event.
-    merchant_id: Option<String>,
-
+    pub merchant_id: Option<String>,
     /// The type of event this represents
-    r#type: Option<PaymentEventPaymentEventType>,
-
+    pub r#type: Option<PaymentEventPaymentEventType>,
     /// A unique ID for the event.
-    event_id: Option<String>,
-
+    pub event_id: Option<String>,
     /// Read only Timestamp of when the event was created, in RFC 3339 format.
     ///
     /// Examples for January 25th, 2020 6:25:34pm Pacific Standard Time:
@@ -21,8 +18,7 @@ pub struct PaymentEventWebhookResponse {
     /// UTC: 2020-01-26T02:25:34Z
     ///
     /// Pacific Standard Time with UTC offset: 2020-01-25T18:25:34-08:00
-    created_at: Option<String>,
-
+    pub created_at: Option<String>,
     /// Data associated with the event.
-    data: Option<PaymentEventData>,
+    pub data: Option<PaymentEventData>,
 }
